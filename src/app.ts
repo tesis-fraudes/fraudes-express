b49721a744d1d55c89d31bb23289f6657790be51
Rodolfo Peña <rodolfo.penia@hotmail.com>
2025-07-06 03:12:29 -0500

    add more logs

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,12 @@ import neuralNetworkRoutes from './modules/neural-network/neural-network.routes'
 //dotenv.config();
 
 const app = express();
+console.log('CLOUDFLARE_ACCESS_KEY:', process.env.CLOUDFLARE_ACCESS_KEY);
+console.log('CLOUDFLARE_SECRET_KEY:', process.env.CLOUDFLARE_SECRET_KEY);
+console.log('CLOUDFLARE_S3_ENDPOINT:', process.env.CLOUDFLARE_S3_ENDPOINT);
+console.log('CLOUDFLARE_BUCKET_NAME:', process.env.CLOUDFLARE_BUCKET_NAME);
 console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
 console.log('PORT:', process.env.PORT);
 
 app.use(json());
