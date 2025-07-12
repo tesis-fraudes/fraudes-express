8e88978f31f198eb2d25dd987059d8b50717e3e6
Rodolfo Peña <rodolfo.penia@hotmail.com>
2025-07-12 12:45:51 -0500

    add script swagger

---
+++ src/scripts/generate-swagger.ts
@@ -0,0 +1,29 @@
+// scripts/generate-swagger.ts
+import fs from 'fs';
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'API de Predicción de Fraudes',
+    version: '1.0.0',
+    description: 'Documentación de la API para gestión de modelos de predicción de fraudes',
+  },
+  servers: [
+    {
+      url: 'https://fd6bat803l.execute-api.us-east-1.amazonaws.com',
+      description: 'Servidor de producción',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ['src/modules/**/*.ts'], // Escanea tus rutas con comentarios JSDoc
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+// ✅ Guardar como JSON para Lambda
+fs.writeFileSync('swagger.json', JSON.stringify(swaggerSpec, null, 2));
+console.log('✅ swagger.json generado con éxito.');
